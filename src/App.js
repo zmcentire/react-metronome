ebb462317a3e1654b277ab545700347855cbcf6e
zmcentire <mcentirezach@gmail.com>
2020-08-13 19:22:03 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Metronome from './Metronome'
+import './App.css'
+
+const App = () => {
+    return(
+        <div>
+            <Metronome />
+        </div>
+    )
+}
+
+export default App
